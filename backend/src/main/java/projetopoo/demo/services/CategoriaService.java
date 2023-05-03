4842dc0d0f018415a1aaca5a76aa7174c3ea42d7
João Darwin <joaodarwin.ip22@gmail.com>
2023-05-03 08:31:56 -0300

    Feat - CategoriaService

---
+++ backend/src/main/java/projetopoo/demo/services/CategoriaService.java
@@ -0,0 +1,65 @@
+package projetopoo.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import projetopoo.demo.entities.Categoria;
+import projetopoo.demo.entities.Tarefa;
+import projetopoo.demo.exceptions.DataBaseException;
+import projetopoo.demo.exceptions.ResourceNotFoundException;
+import projetopoo.demo.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	//Método para listar todas as categorias
+	public List<Categoria> findAll() {
+		List<Categoria> listCategorias = categoriaRepository.findAll();
+		if(listCategorias.isEmpty()) {
+			throw new DataBaseException("Nenhuma categoria foi encontrada");
+		}
+		return listCategorias;
+	}
+	
+	//Método para listar categoria pelo id
+	public Categoria findById(Long id) {
+		try {
+			return categoriaRepository.findById(id).get();
+		} catch(IllegalArgumentException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	//Método para listar tarefas de uma categoria
+	public List<Tarefa> findTarefasByCategoria(Long id) {
+		try {
+			Categoria categoria = findById(id);
+			return categoria.getTarefas();
+		} catch (IllegalArgumentException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	//Método para criar categoria
+	public void insert(Categoria categoria) {
+		try {
+			categoriaRepository.save(categoria);
+		} catch(IllegalArgumentException e) {
+			throw new DataBaseException("A categoria não pode ser null");
+		}
+	}
+	
+	//Método para deletar categoria
+	public void deleteById(Long id) {
+		try {
+			categoriaRepository.deleteById(id);
+		} catch(IllegalArgumentException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+}
