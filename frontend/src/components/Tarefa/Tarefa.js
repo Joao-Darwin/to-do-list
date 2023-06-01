cafe3f91d548d914dbc02b0bc2f0581cb21124c6
João Darwin <joaodarwin.ip22@gmail.com>
2023-06-01 16:13:23 -0300

    Style - Stylização do nome do Status e cor da Tarefa baseado no seu status

--- frontend/src/components/Tarefa/Tarefa.js
+++ frontend/src/components/Tarefa/Tarefa.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import IconeExcluir from "../../assets/iconeExcluir.jpeg"
 
 import "./Tarefa.css"
 import openApi from '../../services/api';
 
 const Tarefa = ({ id, nome, descricao, dataCricao, dataConclusao, importancia, status }) => {
+
+    const [colorBackground] = useState(() => {
+        if (status === 'PRA_FAZER') {
+            return '#ffffff';
+        } else if (status === 'EM_PROGRESSO') {
+            return '#FFD700';
+        } else if (status === 'CONCLUIDA') {
+            return '#2EFF82';
+        } else {
+            return '#DC143C';
+        }
+    });
 
     const deleteTarefa = async () => {
         try {
@@ -18,12 +30,32 @@ const Tarefa = ({ id, nome, descricao, dataCricao, dataConclusao, importancia, s
         }
     }
 
+    // Formatar o Status da Tarefa
+    const statusFormat = (status) => {
+        if(status === 'PRA_FAZER') {
+            return "A FAZER";
+        } else if (status === 'EM_PROGRESSO') {
+            return "EM PROGRESSO";
+        } else if(status === 'CONCLUIDA') {
+            return "CONCLUÍDA";
+        } else {
+            return "CANCELADA";
+        }
+    }
+
+    // Formatar a Data DD/MM/YYYY
+    const dataConclusaoFormat = (data) => {
+        let date = new Date(data);
+        let novaData = `${(date.getDate() + 1) < 10 ? (`0${date.getDate() + 1}`) : (date.getDate() + 1)}/${(date.getMonth() + 2) < 10 ? (`0${date.getMonth() + 2}`) : (date.getMonth() + 2)}/${date.getFullYear()}`
+        return novaData;
+    }
+
     return (
-        <div className='tarefa'>
+        <div className='tarefa' style={{backgroundColor: colorBackground}}>
             <div>
                 <p>Nome: <strong>{nome}</strong></p>
-                <p>Status: <strong>{status}</strong></p>
-                <p>Data conclusão: <strong>{dataConclusao}</strong></p>
+                <p>Status: <strong>{statusFormat(status)}</strong></p>
+                <p>Data conclusão: <strong>{dataConclusaoFormat(dataConclusao)}</strong></p>
             </div>
             <div >
                 <img onClick={deleteTarefa} className='icone' alt='Ícone excluir tarefa' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAWklEQVR4nGNgGAWDGBxnYGD4j4aPUcvww1gMJxcfHiiLD1ErNIYv+E8lPGoxQTAa1P9HExfDaHZiGC1AGEaLTKLAyCsyH1PB0kfkWOxJoeUgSz3IsXgUDA8AAHlcbV33qhrwAAAAAElFTkSuQmCC" />
