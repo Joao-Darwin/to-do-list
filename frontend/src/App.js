0d551a54a22c095e33230de0e8943b9d711ac0d3
João Darwin <joaodarwin.ip22@gmail.com>
2023-06-16 11:42:26 -0300

    Feat - Barra de menu lateral

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,25 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
 import TopBar from './components/TopBar/TopBar';
+import SideBar from './components/SideBar/SideBar';
+import { useState } from 'react';
 
 function App() {
+
+  const [sideBarView, setSideBarView] = useState(false);
+
+    const handleSideBarView = () => {
+        if(sideBarView) {
+            setSideBarView(false);
+        } else {
+            setSideBarView(true);
+        }
+    }
+
   return (
     <BrowserRouter>
-      <TopBar />
+      {sideBarView && <SideBar handleSideBar={handleSideBarView}/>}
+      <TopBar handleSideBar={handleSideBarView}/>
       <Routes/>
     </BrowserRouter>
   );
