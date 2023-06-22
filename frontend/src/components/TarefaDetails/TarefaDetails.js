f71237c8de5eee5a8f14d5fdfbf7f07b703e6625
João Darwin <joaodarwin.ip22@gmail.com>
2023-06-22 16:41:18 -0300

    Feat - Detalhes tarefa

--- frontend/src/components/TarefaDetails/TarefaDetails.js
+++ frontend/src/components/TarefaDetails/TarefaDetails.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import './TarefaDetails.css'
 
-const TarefaDetails = ({idTarefa, nomeTarefa, descricaoTarefa, dataCricaoTarefa, dataConclusaoTarefa, importanciaTarefa, statusTarefa, categoriaTarefa, closeDetailsTarefa}) => {
+const TarefaDetails = ({ idTarefa, nomeTarefa, descricaoTarefa, dataCricaoTarefa, dataConclusaoTarefa, importanciaTarefa, statusTarefa, categoriaTarefa, closeDetailsTarefa }) => {
     return (
         <div className='divTarefaDetails'>
-            <p>Details</p>
-            <p>{idTarefa}</p>
-            <p>{nomeTarefa}</p>
-            <p>{descricaoTarefa}</p>
-            <p>{dataCricaoTarefa}</p>
-            <p>{dataConclusaoTarefa}</p>
-            <p>{importanciaTarefa}</p>
-            <p>{statusTarefa}</p>
-            <p>{categoriaTarefa.nome}</p>
-            <button onClick={closeDetailsTarefa}>fechar</button>
+            <h2>Detalhes da Tarefa <strong>#{idTarefa}</strong></h2>
+            <div className="detailsItem">
+                <strong>Nome da Tarefa:</strong>
+                <p>{nomeTarefa}</p>
+            </div>
+            <div className="detailsItem">
+                <strong>Descrição:</strong>
+                <p>{descricaoTarefa}</p>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'space-between', width: '50%', margin: 'auto'}}>
+                <div className="detailsItem">
+                    <strong>Data de Criação:</strong>
+                    <p>{dataCricaoTarefa}</p>
+                </div>
+                <div className="detailsItem">
+                    <strong>Data de Conclusão:</strong>
+                    <p>{dataConclusaoTarefa}</p>
+                </div>
+            </div>
+            <div className="detailsItem">
+                <strong>Importância:</strong>
+                <p>{importanciaTarefa}</p>
+            </div>
+            <div className="detailsItem">
+                <strong>Status:</strong>
+                <p>{statusTarefa.replace('_', " ")}</p>
+            </div>
+            <div className="detailsItem">
+                <strong>Categoria:</strong>
+                <p>{categoriaTarefa.nome}</p>
+            </div>
+            <button className='buttonFormUpdate buttonFormCancel' onClick={closeDetailsTarefa}>Fechar</button>
         </div>
     )
 }
